7dc367a9198e427db49a2b55f4a42d5856859b6d
Safx <safxdev@gmail.com>
2015-08-23 15:53:23 +0900

    add playground

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Result
+import TypetalkKit
+import XCPlayground
+XCPExecutionShouldContinueIndefinitely()
+XCPSetExecutionShouldContinueIndefinitely()
+
+
+
+TypetalkAPI.setDeveloperSettings(
+    clientId: "your_cliend_id",
+    clientSecret: "your_secret_id",
+    scopes: [Scope.my, Scope.topic_read])
+
+
+
+TypetalkAPI.authorizeWithClientCredentials { (err) -> Void in
+    if err != nil { print(err); return }
+
+    TypetalkAPI.sendRequest(GetTopics()) { result in
+        switch result {
+        case .Success(let response):
+            response.topics.map{$0.topic.name}.map(print)
+        case .Failure(let error):
+            print(error)
+        }
+    }
+}
